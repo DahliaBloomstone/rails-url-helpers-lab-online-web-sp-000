33df71ee2feebd86ddd67da7170e92531b9bf943
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-29 11:59:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :students, only: :index #, as:'students'
 
+ resources :students, only: :show #, as:'student'
+
+
+
 end
