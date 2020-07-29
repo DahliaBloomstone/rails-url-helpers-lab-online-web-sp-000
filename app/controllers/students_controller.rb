36cf1eb16ee09278c6d17150ee742f8e397664b1
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-29 11:52:50 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,13 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @post = Post.find(params[:id])
+
   end
 
   private
